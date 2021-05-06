d2d31e1788530907105fd92e6ed0a3c0e82d9634
sebcampos <sebcampos@criptext.com>
2021-05-06 13:03:21 -0700

    added all_checks.py to check for reboot status, root disk availability, and network connection

---
+++ python_scripts/all_checks.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python3
+import os
+import shutil
+import sys
+import socket
+
+def check_reboot():
+	'''Returns True if the computer has a pending reboot.'''
+	return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+	'''Returns True if there isn't enough disk space, False otherwise.'''
+	du = shutil.disk_usage(disk)
+	#calculate the percentage of free space
+	percent_free = 100 * du.free / du.total
+	#calculate how many free gigabytes
+	gigabytes_free = du.free / 2**30
+	if gigabytes_free < min_gb or percent_free < min_percent:
+		return True
+	else:
+		return False
+
+def check_root_full():
+	'''returns True if the root partition if full, False otherwise.'''
+	return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_no_network():
+	'''Returns True if if fails to resolve Google's URL, False otherwise'''
+	try:
+		socket.gethostbyname("www.google.com")
+		return False
+	except:
+		return True
+
+def main():
+	checks = [
+		(check_reboot, "Pending Reboot"),
+		(check_root_full, "Root partition full"),
+		(check_no_network, "No working network")
+	]
+	everything_ok = True
+	for check, msg in checks:
+		if check():
+			print(msg)
+			everything_ok = False
+	if not everything_ok:
+		sys.exit(1)
+	
+	elif everything_ok == True:
+		print("All checks Passed")
+
+main()
